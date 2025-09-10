9e6abd72cba9921d9e845d074c5e94f421d77405
SebastianP <sebastian.petersson@iths.se>
2025-09-10 15:03:41 +0200

    fram tull uppg 2

--- Exercises/Exercise_0.py
+++ Exercises/Exercise_0.py
@@ -1,1 +1,36 @@
-print("lokalbrorsan")
+#Uppgift 1
+
+print("Uppgift 1a)\n")
+
+import math 
+
+a = 3
+b = 4
+
+c = math.sqrt(a**2 + b**2)
+
+print(f"Med kateterna {a} cm och {b} cm blir hypotenusan {c:.1f} cm.\n")
+
+#_______________________________________________________________________
+
+print("Uppgift 1b)\n")
+
+a = 5.0
+c = 7.0
+
+b = math.sqrt(c**2 - a**2)
+
+print(f"Längden av katetern b är {b:.1f} cm.\n")
+
+#_______________________________________________________________________
+
+print("Uppgift 2)\n")
+
+x = 300
+y = 365
+
+accuracy = 300/365
+procent= accuracy*100
+
+print(f"Träffsäkerheten av modellen är {procent:.2f}%.")
+
