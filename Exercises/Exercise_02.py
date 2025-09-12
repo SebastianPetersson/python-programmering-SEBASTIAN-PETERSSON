f2a349b49917bb0fa1e2ed3bcd5ee5a381cb3222
SebastianP <sebastian.petersson@iths.se>
2025-09-12 09:25:49 +0200

    ex 1

---
+++ Exercises/Exercise_02.py
@@ -0,0 +1,13 @@
+#While_statements_exercise_02
+
+print("Uppgift 1 - Count numbers(*)\n")
+
+i = -10
+
+while i <= 10:
+    print(f"{i}", end = " ")
+    i += 1
+
+#_____________________________________________________________________________
+
+print("\nUppgift 2 - Arithmetic sum (*)\n")
