aa1e601ee39f0debfd4621a46a06d1d9c6367839
SebastianP <sebastian.petersson@iths.se>
2025-09-18 16:28:01 +0200

    Rensad tränings- och testdata, lagt i nya listor.

--- Labs/Labb_2.py
+++ Labs/Labb_2.py
@@ -1,6 +1,26 @@
-# Nu kör vi igång med Labb 2!
+import numpy as np
+import matplotlib.pyplot as plt
 
-with open(r"C:\Users\Sebastian!\Documents\Programmering\python-programmering-SEBASTIAN-PETERSSON\Labs\datapoints.txt", "r") as f:
-    text = f.read()
 
-print("test")
+training_data = []
+test_data = []
+
+#Clean the training data.
+with open(r"C:\Users\Sebastian!\Documents\Programmering\python-programmering-SEBASTIAN-PETERSSON\Labs\datapoints.txt", "r") as tr_file:
+    next(tr_file)
+    for line in tr_file:
+        row = [float(value.strip()) for value in line.split(",")]
+        training_data.append(row)
+
+#Clean the test data.
+with open(r"C:\Users\Sebastian!\Documents\Programmering\python-programmering-SEBASTIAN-PETERSSON\Labs\testpoints.txt", "r") as te_file:
+    next(te_file)
+    for line in te_file:
+        row = [float(value.split(". ", 1)[-1].strip().strip("()")) for value in line.split(",")]
+        test_data.append(row)
+
+
+print(training_data)
+print(test_data)
+
+
