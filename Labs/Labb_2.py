a538d7d4400794e02c4671ce301728ab1abbbe33
SebastianP <sebastian.petersson@iths.se>
2025-09-18 12:25:34 +0200

    Importerat datapoints.

--- Labs/Labb_2.py
+++ Labs/Labb_2.py
@@ -1,9 +1,5 @@
 # Nu kör vi igång med Labb 2!
 
-with open("C:\Users\Sebastian!\Documents\Programmering\python-programmering-SEBASTIAN-PETERSSON\Labs\datapoints.txt", "r") as f:
+with open(r"C:\Users\Sebastian!\Documents\Programmering\python-programmering-SEBASTIAN-PETERSSON\Labs\datapoints.txt", "r") as f:
     text = f.read()
-
-print(repr(text))
-
-
 
