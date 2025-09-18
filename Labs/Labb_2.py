09629b14812f210c005fc8d6b2b25914821ef44c
SebastianP <sebastian.petersson@iths.se>
2025-09-18 09:29:34 +0200

    lagt in alla exercises, börjat på strings & functions

--- Labs/Labb_2.py
+++ Labs/Labb_2.py
@@ -1,1 +1,9 @@
 # Nu kör vi igång med Labb 2!
+
+with open("C:\Users\Sebastian!\Documents\Programmering\python-programmering-SEBASTIAN-PETERSSON\Labs\datapoints.txt", "r") as f:
+    text = f.read()
+
+print(repr(text))
+
+
+
